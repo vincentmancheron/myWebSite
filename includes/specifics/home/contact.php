2971d257595b789f4a5ea75269de070a803cc9c0
Vincent Mancheron <vmancheron@yahoo.fr>
2021-06-29 09:01:17 +0200

    29/06/21 09:01

--- includes/specifics/home/contact.php
+++ includes/specifics/home/contact.php
@@ -1,83 +1,60 @@
-<?php 
-$error = ['empty','regMatch'];
-$error['empty'] = [];
-$error['regMatch'] = [];
+<?php
+// Declarations:
+$error = [];
+$lastname = trim(filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES));
+$firstname = trim(filter_input(INPUT_POST, 'firstname', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES));
+$email = trim(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL, FILTER_FLAG_NO_ENCODE_QUOTES));
+$phoneNumber = trim(filter_input(INPUT_POST, 'phoneNumber', FILTER_SANITIZE_NUMBER_INT, FILTER_FLAG_NO_ENCODE_QUOTES));
+$city = trim(filter_input(INPUT_POST, 'city', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES));
+$state = trim(filter_input(INPUT_POST, 'state', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES));
+$zip = trim(filter_input(INPUT_POST, 'zip', FILTER_SANITIZE_NUMBER_INT, FILTER_FLAG_NO_ENCODE_QUOTES));
+$text = trim(filter_input(INPUT_POST, 'text', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES));
+$cgc = trim(filter_input(INPUT_POST, 'cgc', FILTER_SANITIZE_NUMBER_INT, FILTER_FLAG_NO_ENCODE_QUOTES));
+$vars = ['lastname'=>$lastname,'firstname'=>$firstname,'email'=>$email,'phoneNumber'=>$phoneNumber,'city'=>$city,'state'=>$state,'zip'=>$zip,'text'=>$text,'cgc'=>$cgc];
+$requiredVars = ['lastname'=>$lastname,'firstname'=>$firstname,'email'=>$email,'text'=>$text,'cgc'=>$cgc];
+define('regAlpha','[a-zA-Zéèçàùâêîûüëï ,\'"]*');
+define('regNumber','\d*');
+define('regPhoneNumber','(0|\+33)[1-9]( *[0-9]{2}){4}');
+define('regEmail','[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}');
+// End Declarations
 
-// Vérification Empty:
+// Checking:
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (!empty($_POST['inputLastname'])) {
-        $lastname = $_POST['inputLastname'];
-    } else {
-        $error['empty']['lastname'] = 'Le champ Nom est vide.';
+    // Empty Check:
+    foreach($requiredVars as $key => $value) {
+        if (empty($value)) {
+            $error['empty'][$key] = "";
+        }
     }
+    // End Empty Check
+    // RegMatch Check:
+    foreach($vars as $key => $value) {
+        switch ($value) {
+            case ($value == $lastname || $value == $firstname || $value == $city || $value == $state || $value == $text):
+                if (preg_match('/'.regAlpha.'/',$value)) {
+                    $error['regMatch'][$key] = "";
+                }
+                break;
+            case ($value == $email):
+                if (preg_match('/'.regEmail.'/',$value)) {
+                    $error['regMatch'][$key] = "";
+                }
+                break;
+            case ($value == $phoneNumber || $value == $zip || $value == $cgc):
+                if (preg_match('/'.regNumber.'/',$value)) {
+                    $error['regMatch'][$key] = "";
+                }
+        }
+    }
+    // End RegMatch Check
+    // Length Check:
 
-    if (!empty($_POST['inputFirstname'])) {
-        $firstname = $_POST['inputFirstname'];
-    } else {
-        $error['empty']['firstname'] = 'Le champ Prénom est vide.';
-    }
-
-    if (!empty($_POST['inputEmail'])) {
-        $email = $_POST['inputEmail'];
-    } else {
-        $error['empty']['email'] = 'Le champ Email est vide.';
-    }
-
-    if (!empty($_POST['inputPhoneNumber'])) {
-        $phoneNumber = $_POST['inputPhoneNumber'];
-    } else {
-        $error['empty']['phoneNumber'] = 'Le champ Numéro de Téléphone est vide.';
-    }
-
-    if (!empty($_POST['inputCity'])) {
-        $city = $_POST['inputCity'];
-    } else {
-        $error['empty']['city'] = 'Le champ Ville est vide.';
-    }
-
-    if (!empty($_POST['inputState'])) {
-        $state = $_POST['inputState'];
-    } else {
-        $error['empty']['state'] = 'Le champ Pays est vide.';
-    }
-
-    if (!empty($_POST['inputZip'])) {
-        $zip = $_POST['inputZip'];
-    } else {
-        $error['empty']['zip'] = 'Le champ Zip est vide.';
-    }
-
-    if (!empty($_POST['inputMessage'])) {
-        $message = $_POST['inputMessage'];
-    } else {
-        $error['empty']['message'] = 'Le champ Message est vide.';
-    }
-
-    if (!empty($_POST['gridCheck'])) {
-        $gridCheck = $_POST['gridCheck'];
-    } else {
-        $error['empty']['gridCheck'] = 'Il faut lire et accepter les CGCs.';
-    }
+    // End Length Check
 }
-
-// Vérification Regex:
-$regAlpha = '[a-zA-Z]*';
-$regZip = '\d{5}';
-$regPhoneNumber = '(0|\+33)[1-9]( *[0-9]{2}){4}';
-$regEmail = '[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}';
-
-// if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-//     if (!empty($_POST['lastname'])) {
-//         preg_match($regAlpha, $lastname, $answer, PREG_OFFSET_CAPTURE);
-//         if ($answer) {
-//             $error['regMatch']['lastname'] = false;
-//         }
-//     }
-// }
+// End Checking
 ?>
 
 <section id="contact" class="row">
-    <?php var_dump($error['regMatch']) ?>
     <h3>Contact</h3>
     <div class="row justify-content-around">
         <div class="col-12 col-md-5">
@@ -109,75 +86,76 @@ $regEmail = '[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}';
             </div>
         </div>
 
-        <form id="contactForm" class="col-12 col-md-5" action="index.php#contactForm" method="POST">
+        <form id="contactForm" class="col-12 col-md-5" action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>"
+            method="POST">
             <div class="form-row">
                 <div class="form-group col-md-6 mx-3 mx-md-0">
-                    <label for="inputLastname">Nom</label>
-                    <input type="text" class="form-control" id="inputLastname" name="inputLastname"
-                        pattern="<?=$regAlpha?>" title="N'utilisez que des lettres."
-                        value="<?= $lastname ?? '' ?>" placeholder="Mancheron">
+                    <label for="lastname">Nom</label>
+                    <input 'required' type="text" class="form-control" id="lastname" name="lastname"
+                        pattern="<?=regAlpha?>" title="N'utilisez que des lettres." minlength="2" maxlength="50" value="<?= $lastname ?? '' ?>"
+                        placeholder="Mancheron">
                     <div class="error"><?= $error['empty']['lastname'] ?? '' ?></div>
                 </div>
                 <div class="form-group col-md-6 mx-3 mx-md-0">
-                    <label for="inputFirstname">Prénom</label>
-                    <input type="text" class="form-control" id="inputFirstname" name="inputFirstname"
-                        pattern="<?=$regAlpha?>" title="N'utilisez que des lettres." value="<?= $firstname ?? '' ?>" placeholder="Vincent">
+                    <label for="firstname">Prénom</label>
+                    <input 'required' type="text" class="form-control" id="firstname" name="firstname"
+                        pattern="<?=regAlpha?>" title="N'utilisez que des lettres." minlength="2" maxlength="50" value="<?= $firstname ?? '' ?>"
+                        placeholder="Vincent">
                     <div class="error"><?= $error['empty']['firstname'] ?? '' ?></div>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6 mx-3 mx-md-0">
-                    <label for="inputEmail">Email</label>
-                    <input type="email" class="form-control" id="inputEmail" name="inputEmail"
-                        pattern="$<?=$regEmail?>" value="<?= $email ?? '' ?>" placeholder="vmancheron@yahoo.fr">
+                    <label for="email">Email</label>
+                    <input 'required' type="email" class="form-control" id="email" name="email"
+                        value="<?= $email ?? '' ?>" placeholder="vmancheron@yahoo.fr">
                     <div class="error"><?= $error['empty']['email'] ?? '' ?></div>
                 </div>
                 <div class="form-group col-md-6 mx-3 mx-md-0">
-                    <label for="inputPhoneNumber">Téléphone</label>
-                    <input type="text" class="form-control" id="inputPhoneNumber" name="inputPhoneNumber"
-                        pattern="<?=$regPhoneNumber?>"
-                        title="Indiquez une série de 10 chiffres sans caractère spéciaux."
-                        value="<?= $phoneNumber ?? '' ?>" placeholder="07 88 22 87 29">
+                    <label for="phoneNumber">Téléphone</label>
+                    <input type="text" class="form-control" id="phoneNumber" name="phoneNumber"
+                        pattern="<?=regPhoneNumber?>" title="Indiquez une série de 10 chiffres sans caractère spéciaux."
+                        minlength="10" maxlength="10" value="<?= $phoneNumber ?? '' ?>" placeholder="07 88 22 87 29">
                     <div class="error"><?= $error['empty']['phoneNumber'] ?? '' ?></div>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6 mx-3 mx-md-0">
-                    <label for="inputCity">Ville</label>
-                    <input type="text" class="form-control" id="inputCity" name="inputCity"
-                        pattern="<?=$regAlpha?>" title="N'utilisez que des lettres." value="<?= $city ?? '' ?>" placeholder="Amiens">
+                    <label for="city">Ville</label>
+                    <input type="text" class="form-control" id="city" name="city" pattern="<?=regAlpha?>"
+                        title="N'utilisez que des lettres." minlength="2" maxlength="50" value="<?= $city ?? '' ?>" placeholder="Amiens">
                     <div class="error"><?= $error['empty']['city'] ?? '' ?></div>
                 </div>
                 <div class="form-group col-md-4 mx-3 mx-md-0">
-                    <label for="inputState">Pays</label>
-                    <select id="inputState" name="inputState" class="form-control">
+                    <label for="state">Pays</label>
+                    <select id="state" name="state" minlength="2" maxlength="50" class="form-control">
                         <option><?= $state ?? 'Choisir' ?></option>
                     </select>
                     <div class="error"><?= $error['empty']['state'] ?? '' ?></div>
                 </div>
                 <div class="form-group col-md-2 mx-3 mx-md-0">
-                    <label for="inputZip">Zip</label>
-                    <input type="text" class="form-control" id="inputZip" name="inputZip"
-                        pattern="<?=$regZip?>" title="Indiquez une série de 5 chiffres."
-                        value="<?= $zip ?? '' ?>" placeholder="80000">
+                    <label for="zip">Zip</label>
+                    <input type="text" class="form-control" id="zip" name="zip" pattern="<?=regZip?>"
+                        title="Indiquez une série de 5 chiffres." minlength="5" maxlength="5" value="<?= $zip ?? '' ?>" placeholder="80000">
                     <div class="error"><?= $error['empty']['zip'] ?? '' ?></div>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-11 mx-auto mx-md-0">
-                    <label for="inputText">Votre message</label>
-                    <textarea class="form-control" id="inputText" name="inputText" value="<?= $message ?? '' ?>"></textarea>
+                    <label for="text">Votre message</label>
+                    <textarea 'required' class="form-control" id="text" name="text"
+                        value="<?= $message ?? '' ?>"></textarea>
                     <div class="error"><?= $error['empty']['message'] ?? '' ?></div>
                 </div>
             </div>
             <div class="form-group">
                 <div class="form-check ml-3 ml-md-0">
-                    <input class="form-check-input" type="checkbox" id="gridCheck" name="gridCheck"
-                        value="Done">
-                    <label class="form-check-label" for="gridCheck">
+                    <input 'required' class="form-check-input" type="checkbox" id="cgc" name="cgc"
+                        value="1">
+                    <label class="form-check-label" for="cgc">
                         Je suis en accord avec les <a href=>CGC</a>.
                     </label>
-                    <div class="error"><?= $error['empty']['gridCheck'] ?? '' ?></div>
+                    <div class="error"><?= $error['empty']['cgc'] ?? '' ?></div>
                 </div>
             </div>
             <button type="submit" class="btn btn-primary ml-3 ml-md-0">Soumettre</button>
